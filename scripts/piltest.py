71240b0389fbcd766bb3d32d540b35dbaec8c76a
uberlieder <uberlieder@gmail.com>
2014-02-04 10:13:27 -0500

    Now changes the grey level of the pixels

--- scripts/piltest.py
+++ scripts/piltest.py
@@ -4,12 +4,12 @@ original = Image.open("images/books-edited.jpg") # load an image from the hard d
 changed = original.convert("L") # Convert from RGB to YCbCr
 pixarr = changed.load() # load pixels from image into array
 
-#for j in range(changed.size[1]):
-	#for i in range (changed.size[0]):
+for j in range(changed.size[1]): # For every single pixel
+	for i in range (changed.size[0]):
 		#print(pixarr[i,j])
-		#tup = pixarr[i,j]
-		#pixarr[i,j] = (255,tup[1],tup[2]) 
+		pixarr[i,j] = 125 # Change the pixel to grey
+		#print(pixarr[i,j])
 
-changed.show() # will print just in greyscale
+changed.show() # will print just grey
 
 # helpful documentation for this library @ http://effbot.org/imagingbook/image.htm
