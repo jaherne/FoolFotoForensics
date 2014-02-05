5b88d12936de2de452e3a25c012ec1ce22ded04e
uberlieder <uberlieder@gmail.com>
2014-02-05 15:24:18 -0500

    Saves changed image as a different quality

--- scripts/piltest.py
+++ scripts/piltest.py
@@ -1,20 +1,23 @@
+#!/usr/bin/env python
 from PIL import Image, ImageFilter
  
 original = Image.open("images/books-edited.jpg") # load an image from the hard drive
 changed = original.convert("YCbCr") # Convert from RGB to YCbCr
+changed.show()
 pixarr = changed.load() # load pixels from image into array
 
 for j in range(changed.size[1]): # For every single pixel
 	for i in range (changed.size[0]):
-		#print(pixarr[i,j])
+		#print(pixarr[i,j])l
 		place=pixarr[i,j][1]
 		holder=pixarr[i,j][2]
-		pixarr[i,j] = (0,place,holder) # Change the luminance
+		#pixarr[i,j] = (0,place,holder) # Change the luminance
 		#print(pixarr[i,j])
 
-changed.show() # will print it as dark
+#changed.show() # will print it as dark
 
-new = changed.convert("RGB") # Convert back to RGB
-new.show() # Should be equal to the changed one 
+changed.save("images/test.jpg", quality=5)
+
+new = Image.open("images/test.jpg").show()
 
 # helpful documentation for this library @ http://effbot.org/imagingbook/image.htm
